7ef23fb391e23309763bc7302c222d7bf9e44cdc
Amber Jen <amberjen226@gmail.com>
2024-05-30 10:29:58 +0800

    Set up basic scene and controls

--- 01-solar-system/main.js
+++ 01-solar-system/main.js
@@ -1,1 +1,61 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Pane } from 'tweakpane';
+
+// Initialize pane
+const pane = new Pane();
+
+
+// Initialize the scene
+const scene = new THREE.Scene();
+
+
+// TEMP: Objects
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x5a3ff2 });
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
+
+
+// Initialize the camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  400
+);
+
+camera.position.y = 5;
+camera.position.z = 100;
+
+// Initialize the renderer
+const canvas = document.querySelector('canvas.threejs');
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Add controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.minDistance = 5;
+controls.maxDistance = 200;
+
+// Add resize listener
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+// Render the scene (i.e. animate)
+const renderloop = () => {
+  window.requestAnimationFrame(renderloop);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+renderloop();
