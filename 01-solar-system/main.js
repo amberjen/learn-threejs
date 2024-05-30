6179d976b8aa1cc035e6c33d42d8092c606db4ce
Amber Jen <amberjen226@gmail.com>
2024-05-30 23:15:33 +0800

    feat: add Sun, Moon, and Earth objects

--- 01-solar-system/main.js
+++ 01-solar-system/main.js
@@ -2,22 +2,50 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Pane } from 'tweakpane';
 
+// -----------------------------------------
 // Initialize pane
+// -----------------------------------------
 const pane = new Pane();
 
-
+// -----------------------------------------
 // Initialize the scene
+// -----------------------------------------
 const scene = new THREE.Scene();
 
+// -----------------------------------------
+// Add objects
+// -----------------------------------------
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 
-// TEMP: Objects
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x5a3ff2 });
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cubeMesh);
+// Sun
+const sunMaterial = new THREE.MeshBasicMaterial({
+  color: 'yellow'
+});
+const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
+sun.scale.setScalar(5);
+scene.add(sun);
+
+// Earth
+const earthMaterial = new THREE.MeshBasicMaterial({
+  color: 'blue'
+});
+const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
+earth.position.x = 10;
+scene.add(earth);
+
+// Moon
+const moonMaterial = new THREE.MeshBasicMaterial({
+  color: 'wheat'
+});
+const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
+moon.scale.setScalar(0.3);
+moon.position.x = 2;
+earth.add(moon);
 
 
+// -----------------------------------------
 // Initialize the camera
+// -----------------------------------------
 const camera = new THREE.PerspectiveCamera(
   35,
   window.innerWidth / window.innerHeight,
@@ -28,7 +56,9 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.y = 5;
 camera.position.z = 100;
 
+// -----------------------------------------
 // Initialize the renderer
+// -----------------------------------------
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({
   canvas,
@@ -37,21 +67,26 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// -----------------------------------------
 // Add controls
+// -----------------------------------------
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.minDistance = 5;
 controls.maxDistance = 200;
 
+// -----------------------------------------
 // Add resize listener
+// -----------------------------------------
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-
+// -----------------------------------------
 // Render the scene (i.e. animate)
+// -----------------------------------------
 const renderloop = () => {
   window.requestAnimationFrame(renderloop);
   controls.update();
