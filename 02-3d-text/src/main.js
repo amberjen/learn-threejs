683f56054689a1d2e309a1e18011a77467b9ac63
Amber Jen <amberjen226@gmail.com>
2024-06-11 14:49:54 +0800

    feat: complete project 2

--- 02-3d-text/src/main.js
+++ 02-3d-text/src/main.js
@@ -1,11 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import GUI from 'lil-gui';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+// import GUI from 'lil-gui';
 
 // ----------------------------------
 // Debug
 // ----------------------------------
-const gui = new GUI();
+// const gui = new GUI();
 
 // ----------------------------------
 // Canvas
@@ -16,27 +19,109 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 // ----------------------------------
 const scene = new THREE.Scene();
+// const rgbeLoader = new RGBELoader();
+// rgbeLoader.load(
+//   'textures/environment/clear_puresky_2k.hdr',
+//   (texture) => {
+//     texture.mapping = THREE.EquirectangularReflectionMapping;
+//     scene.background = texture;
+//     scene.environment = texture;
+//   }
+// );
+
 
 // ----------------------------------
 // Textures
 // ----------------------------------
 const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load('textures/matcaps/3.png');
+
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
+
+const matcapMaterial = new THREE.MeshMatcapMaterial({
+  matcap: matcapTexture
+});
+
+// ----------------------------------
+// Fonts
+// ----------------------------------
+const fontLoader = new FontLoader();
+fontLoader.load(
+  'fonts/Dryouth_Regular.json',
+  (font) => {
+    const title = 'DIGITAL\nCREATIVE\n@AMBERJEN';
+
+    // Text Geometry
+    const textGeometry = new TextGeometry(title, {
+      font: font,
+      size: 0.5,
+      depth: 0.2,
+      curveSegments: 5,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 4
+    });
+
+    textGeometry.center();
+
+    // Text Mesh
+    const textMesh = new THREE.Mesh(textGeometry, matcapMaterial);
+
+    scene.add(textMesh);
+
+  }
+)
 
 // ----------------------------------
 // Objects
 // ----------------------------------
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xffffff })
-);
+const torusGeometry = new THREE.TorusGeometry(0.2, 0.125, 16, 48);
+const boxGeometry = new THREE.BoxGeometry(0.35, 0.35, 0.35);
+const sphereGeometry = new THREE.SphereGeometry(0.2);
+const coneGeometry = new THREE.ConeGeometry(0.2, 0.5, 32);
 
-scene.add(cube);
+const generateRandomObjects = (objGeometry, objMaterial, objAmount, objPosition) => {
+
+  for(let i=0; i<objAmount; i++) {
+    const objMesh = new THREE.Mesh(objGeometry, objMaterial);
+  
+    // Randomize position
+    objMesh.position.x = (Math.random() - 0.5) * objPosition;
+    objMesh.position.y = (Math.random() - 0.5) * objPosition;
+    objMesh.position.z = (Math.random() - 0.5) * objPosition;
+    
+    // Randomize rotation
+    objMesh.rotation.x = Math.random() * Math.PI;
+    objMesh.rotation.y = Math.random() * Math.PI;
+    // objMesh.rotation.z = Math.random() * Math.PI;
+    
+    // Randomize scale
+    const objScale = Math.random();
+    objMesh.scale.set(objScale, objScale, objScale);
+  
+    scene.add(objMesh);
+  }
+
+}
+
+generateRandomObjects(torusGeometry, matcapMaterial, 55, 11);
+generateRandomObjects(boxGeometry, matcapMaterial, 25, 11);
+generateRandomObjects(sphereGeometry, matcapMaterial, 15, 12);
+generateRandomObjects(coneGeometry, matcapMaterial, 15, 13);
+
+// ----------------------------------
+// Light
+// ----------------------------------
+const pointLight = new THREE.PointLight(0xffffff, 2000);
+scene.add(pointLight)
 
 // ----------------------------------
 // Helpers
 // ----------------------------------
-const axesHelper =  new THREE.AxesHelper(2);
-scene.add(axesHelper);
+// const axesHelper =  new THREE.AxesHelper(2);
+// scene.add(axesHelper);
 
 // ----------------------------------
 // Sizes
@@ -63,8 +148,8 @@ scene.add(camera);
 // ----------------------------------
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.minDistance = 1.5;
-controls.maxDistance = 100;
+controls.minDistance = 2;
+controls.maxDistance = 25;
 
 // ----------------------------------
 // Renderer
@@ -106,7 +191,7 @@ const animate = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update obejcts
-  cube.rotation.y = elapsedTime * 0.5;
+
 
   // Update controls
   controls.update();
