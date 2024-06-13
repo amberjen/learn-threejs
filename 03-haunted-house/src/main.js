67a74994f896749851d0b3ee034568f5283dbc1e
Amber Jen <amberjen226@gmail.com>
2024-06-13 21:34:15 +0800

    feat: add ghost lights

--- 03-haunted-house/src/main.js
+++ 03-haunted-house/src/main.js
@@ -308,6 +308,12 @@ const doorLight = new THREE.PointLight(0xff7d46, 5);
 doorLight.position.set(0, 2.2, 2.5);
 house.add(doorLight);
 
+// Ghost Lights
+const ghost1 = new THREE.PointLight(0x1450f5, 5);
+const ghost2 = new THREE.PointLight(0x9016fa, 5);
+const ghost3 = new THREE.PointLight(0xe37b20, 5);
+scene.add(ghost1, ghost2, ghost3);
+
 
 // ----------------------------------
 // Sizes
@@ -380,7 +386,23 @@ const animate = () => {
   timer.update();
   const elapsedTime = timer.getElapsed();
 
-  // Update obejcts
+  // Animate ghosts
+  const ghost1Angle = elapsedTime * 0.5;
+  ghost1.position.x = Math.sin(ghost1Angle) * 4;
+  ghost1.position.z = Math.cos(ghost1Angle) * 4;
+  ghost1.position.y = Math.sin(ghost1Angle) * Math.sin(ghost1Angle * 2.34) * Math.sin(ghost1Angle * 3.45);
+
+  const ghos21Angle = -(elapsedTime * 0.37);
+  ghost2.position.x = Math.sin(ghos21Angle) * 5;
+  ghost2.position.z = Math.cos(ghos21Angle) * 5;
+  ghost2.position.y = Math.sin(ghos21Angle) * Math.sin(ghos21Angle * 2.34) * Math.sin(ghos21Angle * 3.45);
+
+  const ghost3Angle = elapsedTime * 0.21;
+  ghost3.position.x = Math.sin(ghost3Angle) * 6;
+  ghost3.position.z = Math.cos(ghost3Angle) * 6;
+  ghost3.position.y = Math.sin(ghost3Angle) * Math.sin(ghost3Angle * 2.34) * Math.sin(ghost3Angle * 3.45);
+
+
 
 
   // Update controls
