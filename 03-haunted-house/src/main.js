cfba46fbb2e34711164a474458a9a9d2643503a5
Amber Jen <amberjen226@gmail.com>
2024-06-13 14:24:48 +0800

    feat: add grave texturing

--- 03-haunted-house/src/main.js
+++ 03-haunted-house/src/main.js
@@ -104,6 +104,17 @@ bushColorTexture.wrapS = THREE.RepeatWrapping;
 bushARMTexture.wrapS = THREE.RepeatWrapping;
 bushNormalTexture.wrapS = THREE.RepeatWrapping;
 
+// Grave
+const graveColorTexture = textureLoader.load('grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.jpg');
+const graveARMTexture = textureLoader.load('grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.jpg');
+const graveNormalTexture = textureLoader.load('grave/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.jpg');
+
+graveColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+graveColorTexture.repeat.set(0.3, 0.4);
+graveARMTexture.repeat.set(0.3, 0.4);
+graveNormalTexture.repeat.set(0.3, 0.4);
+
 
 // ----------------------------------
 // Objects
@@ -247,7 +258,13 @@ const graves = new THREE.Group();
 scene.add(graves);
 
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
-const graveMaterial = new THREE.MeshStandardMaterial();
+const graveMaterial = new THREE.MeshStandardMaterial({
+  map: graveColorTexture,
+  aoMap: graveARMTexture,
+  roughnessMap: graveARMTexture,
+  metalnessMap: graveARMTexture,
+  normalMap: graveNormalTexture
+});
 
 for(let i=0; i<30; i++) {
   
