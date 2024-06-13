174ecd0efb1328c10b917fe42faa4fa0b9b24779
Amber Jen <amberjen226@gmail.com>
2024-06-13 14:16:14 +0800

    feat: add bush texturing

--- 03-haunted-house/src/main.js
+++ 03-haunted-house/src/main.js
@@ -89,6 +89,22 @@ roofColorTexture.wrapT = THREE.RepeatWrapping;
 roofARMTexture.wrapT = THREE.RepeatWrapping;
 roofNormalTexture.wrapT = THREE.RepeatWrapping;
 
+// Bush
+const bushColorTexture = textureLoader.load('bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.jpg');
+const bushARMTexture = textureLoader.load('bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.jpg');
+const bushNormalTexture = textureLoader.load('bush/leaves_forest_ground_1k/leaves_forest_ground_nor_gl_1k.jpg');
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+bushColorTexture.repeat.set(2, 1);
+bushARMTexture.repeat.set(2, 1);
+bushNormalTexture.repeat.set(2, 1);
+
+bushColorTexture.wrapS = THREE.RepeatWrapping;
+bushARMTexture.wrapS = THREE.RepeatWrapping;
+bushNormalTexture.wrapS = THREE.RepeatWrapping;
+
+
 // ----------------------------------
 // Objects
 // ----------------------------------
@@ -196,24 +212,33 @@ house.add(door);
 // House Component: Bushes
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
 const bushMaterial = new THREE.MeshStandardMaterial({
-  color: 0x038a32 // TEMP:
+  color: 0x62a363,
+  map: bushColorTexture,
+  aoMap: bushARMTexture,
+  roughnessMap: bushARMTexture,
+  metalnessMap: bushARMTexture,
+  normalMap: bushNormalTexture
 });
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush1.scale.setScalar(0.5);
 bush1.position.set(1.2, 0.2, 2.5)
+bush1.rotation.x = -0.75;
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush2.scale.setScalar(0.25);
-bush2.position.set(1.8, 0.1, 2.3)
+bush2.position.set(1.8, 0.1, 2.3);
+bush2.rotation.x = -0.75;
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush3.scale.setScalar(0.4);
-bush3.position.set(-1.2, 0.1, 2.5)
+bush3.position.set(-1.2, 0.1, 2.5);
+bush3.rotation.x = -0.75;
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush4.scale.setScalar(0.3);
-bush4.position.set(-1.85, 0.05, 2.4)
+bush4.position.set(-1.85, 0.05, 2.4);
+bush4.rotation.x = -0.75;
 
 house.add(bush1, bush2, bush3, bush4);
 
